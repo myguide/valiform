2b7948642884a2fd35abcd3afe400823b8aa2056
Harry Lawrence <hazbo@gmx.com>
2015-08-20 14:55:12 +0100

    Tabs to spaces

--- src/valiform_rule.ts
+++ src/valiform_rule.ts
@@ -16,65 +16,65 @@
 
 class ValiformRule
 {
-	/**
-	 * @var any The input element within the form
-	 * @var string The key of the rule to be applied
-	 * @var any The value of the rule to be applied
-	 */
-	private inputElement: any;
-	private rule: string;
-	private value: any;
+    /**
+     * @var any The input element within the form
+     * @var string The key of the rule to be applied
+     * @var any The value of the rule to be applied
+     */
+    private inputElement: any;
+    private rule: string;
+    private value: any;
 
-	/**
-	 * constructor gives all properties of this class a default value, passed
-	 * through once instanciated.
-	 *
-	 * @param any The form input element
-	 * @param string The key of the rule
-	 * @param any The value of the rule
-	 *
-	 * @return void
-	 */
-	constructor(inputElement: any, rule: string, value: any)
-	{
-		this.inputElement = inputElement;
-		this.rule = rule;
-		this.value = value;
-	}
+    /**
+     * constructor gives all properties of this class a default value, passed
+     * through once instanciated.
+     *
+     * @param any The form input element
+     * @param string The key of the rule
+     * @param any The value of the rule
+     *
+     * @return void
+     */
+    constructor(inputElement: any, rule: string, value: any)
+    {
+        this.inputElement = inputElement;
+        this.rule = rule;
+        this.value = value;
+    }
 
-	/**
-	 * validate check the rule by its key and calls the method that matches with
-	 * that key.
-	 *
-	 * @return boolean
-	 */
-	public validate(): boolean {
-		switch (this.rule) {
-			case "required":
-				return this.required(this.value);
-				break;
-		}
-		return false;
-	}
+    /**
+     * validate check the rule by its key and calls the method that matches with
+     * that key.
+     *
+     * @return boolean
+     */
+    public validate(): boolean {
+        switch (this.rule) {
+            case "required":
+                return this.required(this.value);
+                break;
+        }
+        return false;
+    }
 
-	/**
-	 * requird is a rule that verifies if a field has any input in it at all
-	 *
-	 * @param boolean The rule value
-	 *
-	 * @return boolean
-	 */
-	private required(is: boolean): boolean
-	{
-		if (this.inputElement.value === "" && is === true) {
-			// Add error message to an array
-			return false;
-		}
-		return true;
-	}
+    /**
+     * requird is a rule that verifies if a field has any input in it at all
+     *
+     * @param boolean The rule value
+     *
+     * @return boolean
+     */
+    private required(is: boolean): boolean
+    {
+        if (this.inputElement.value === "" && is === true) {
+            // Add error message to an array
+            return false;
+        }
+        return true;
+    }
 
-	private type(is: string)
-	{
+    private type(is: string)
+    {
 
-	}
+    }
 }
